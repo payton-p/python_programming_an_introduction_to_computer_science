3aafe196e3923b64da6de90f398417baddb12daa
Payton Pietron <paytonpietron@gmail.com>
2022-12-14 00:00:33 -0500

    Change comment type and move graphics

--- chapter1_computers_and_programs/chaos-function-example.py
+++ chapter1_computers_and_programs/chaos-function-example.py
@@ -1,21 +1,19 @@
-"""
-"Python creates a companion file with a .pyc extension. This is an intermediate file used by the Python 
-interpreter. Technically, Python uses a hybrid compiling/interpreting process. The Python source in the 
-module file is compiled into more primitve instructions called byte code. This byte code (the .pyc file) 
-is then interpreted." (pg. 14)
+# "Python creates a companion file with a .pyc extension. This is an intermediate file used by the Python
+# interpreter. Technically, Python uses a hybrid compiling/interpreting process. The Python source in the
+# module file is compiled into more primitve instructions called byte code. This byte code (the .pyc file)
+# is then interpreted." (pg. 14)
 
-" . . . the chaos program illustrates an interesting phenomenon . . . you'll find that no matter what 
-number you start with, the results are always similar: the program spits back 10 seemingly random 
-numbers between 0 and 1. As the program runs, the value of x seems to jump around, well, chaotically 
-. . . The function computed by this program has the general form: k(x)(1 - x), where k in this case 
-is 3.9. This is called a logistic function. It models certain kinds of unstable electronic circuits
-and is also sometimes used to predict population under limiting conditions. Repeated application 
-of the logistic function can produce chaos. Although our program has a well-defined underlying behavior, 
-the output seems unpredictable." (pg. 18)
+# " . . . the chaos program illustrates an interesting phenomenon . . . you'll find that no matter what
+# number you start with, the results are always similar: the program spits back 10 seemingly random
+# numbers between 0 and 1. As the program runs, the value of x seems to jump around, well, chaotically
+# . . . The function computed by this program has the general form: k(x)(1 - x), where k in this case
+# is 3.9. This is called a logistic function. It models certain kinds of unstable electronic circuits
+# and is also sometimes used to predict population under limiting conditions. Repeated application
+# of the logistic function can produce chaos. Although our program has a well-defined underlying behavior,
+# the output seems unpredictable." (pg. 18)
 
-Chaos Theory: Where a tiny error or uncertainty in input measurements can cause a massive error in output, 
-this is a chaotic function.
-"""
+# Chaos Theory: Where a tiny error or uncertainty in input measurements can cause a massive error in output,
+# this is a chaotic function.
 
 
 def main():

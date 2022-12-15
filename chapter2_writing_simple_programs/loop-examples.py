6130a7bb0d337e1c464592d44fad660dd3633bf3
Payton Pietron <paytonpietron@gmail.com>
2022-12-14 21:03:00 -0500

    Simple loop examples

---
+++ chapter2_writing_simple_programs/loop-examples.py
@@ -0,0 +1,19 @@
+def main():
+    # Definite loop example. This particular loop pattern is called a counted loop.
+    # The variable after the keyword "for" is called the loop index. The
+    # function range is used for generating a sequence of numbers on the fly.
+    for i in range(10):
+        print(i)
+    print("End of the first loop example.\n")
+
+    # Another loop example
+    for i in [0, 1, 3, 5]:
+        print(i)
+    print("End of the second loop example.\n")
+
+    # Compare what these prints return
+    print(range(3))
+    print(list(range(3)))  # turns it into an explicit list
+
+
+main()

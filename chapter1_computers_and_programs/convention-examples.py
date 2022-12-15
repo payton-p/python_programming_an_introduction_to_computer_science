b3c3e8933f382905203d92f535f9666d0b55ff64
Payton Pietron <paytonpietron@gmail.com>
2022-12-14 23:08:23 -0500

    Add notes

---
+++ chapter1_computers_and_programs/convention-examples.py
@@ -0,0 +1,10 @@
+def main():
+    _internal_use_variable = "The more you know."
+    print(_internal_use_variable)
+    print("The underscore prefix is meant as a hint to another programmer that a " +
+          "variable or method starting with a single underscore is intended for internal " +
+          "use. This convention is defined in PEP 8. This isn't enforced by Python. Python " +
+          "does not have strong distinctions between “private” and “public” variables like Java does.")
+
+
+main()

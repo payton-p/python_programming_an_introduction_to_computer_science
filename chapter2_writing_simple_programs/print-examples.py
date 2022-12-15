3bd9d54872e1202157365229848c7ab77ec6bb4b
Payton Pietron <paytonpietron@gmail.com>
2022-12-14 20:43:20 -0500

    Print examples

---
+++ chapter2_writing_simple_programs/print-examples.py
@@ -0,0 +1,15 @@
+# Notes: Python uses garbage collection (aka the process of automatic memory management). 
+
+def main():
+    print("Shall we look at some examples?")
+
+    # The causes a carriage return (aka newline) 
+    print()
+    print("The line right above this one is from an empty print statement.\n")
+
+    # The end default is a carriage return (\n), but you can explicitly set it.
+    print("This print call shows how you can determine what comes at the end. The default is \\n, " +
+          "but we are using k instead", end="k")
+
+
+main()

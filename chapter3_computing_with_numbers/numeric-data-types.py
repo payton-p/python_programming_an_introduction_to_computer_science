c1bfa5ce88aff9e92e54e1657f3c0259ad9bf976
Payton Pietron <paytonpietron@gmail.com>
2022-12-16 20:18:56 -0500

    Examples of how floats and ints behave

---
+++ chapter3_computing_with_numbers/numeric-data-types.py
@@ -0,0 +1,27 @@
+def main():
+    # Integer example
+    integer = 3
+    print(type(integer))
+
+    # Float example
+    float_num = 4.0
+    print(type(float_num))
+
+    # Normal division always returns a float
+    normal_division_example = 3 / 2
+    print(normal_division_example)
+
+    # Integer division with two ints
+    integer_division_example = 11 // 5
+    print(integer_division_example)
+
+    # Integer division with two floats
+    integer_division_example = 11.0 // 5.0
+    print(integer_division_example)
+
+    # Integer division with one int and one float, the result is cast to float
+    integer_division_example = 11 // 5.0
+    print(integer_division_example)
+
+
+main()

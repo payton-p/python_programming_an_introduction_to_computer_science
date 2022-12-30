4388313bc6b02a26315b984f5b3ff8d805dfc9d6
Payton Pietron <paytonpietron@gmail.com>
2022-12-30 18:48:46 -0500

    Clean up _Transform class

--- chapter0_provided_graphics/graphics/_transform.py
+++ chapter0_provided_graphics/graphics/_transform.py
@@ -1,22 +1,23 @@
 class _Transform:
     """Internal class for 2-D coordinate transformations."""
 
-    def __init__(self, w, h, xlow, ylow, xhigh, yhigh):
-        # w, h are width and height of the window
-        # (xlow,ylow) are coordinates of lower-left [raw (0,h-1)]
-        # (xhigh,yhigh) are coordinates of upper-right [raw (w-1,0)]
-        xspan = (xhigh - xlow)
-        yspan = (yhigh - ylow)
-        self.xbase = xlow
-        self.ybase = yhigh
-        self.xscale = xspan / float(w - 1)
-        self.yscale = yspan / float(h - 1)
+    def __init__(self, window_width, window_height, x_lower_left, y_lower_left, x_upper_right, y_upper_right):
+        """Construct a new _Transform object."""
+
+        # (x_lower_left, y_lower_left) are coordinates of lower-left, raw (0, window_height-1).
+        # (x_upper_right, y_upper_right) are coordinates of upper-right, raw (window_width-1, 0).
+        x_span = (x_upper_right - x_lower_left)
+        y_span = (y_upper_right - y_lower_left)
+        self.x_base = x_lower_left
+        self.y_base = y_upper_right
+        self.x_scale = x_span / float(window_width - 1)
+        self.y_scale = y_span / float(window_height - 1)
 
     def get_screen_coords(self, x, y):
         """Return x,y in screen (actual window) coordinates."""
 
-        xs = (x - self.xbase) / self.xscale  # the "s" in xs is referencing the x screen coordinate, same for y
-        ys = (self.ybase - y) / self.yscale
+        xs = (x - self.x_base) / self.x_scale  # the "s" in xs is referencing the x screen coordinate, same for y
+        ys = (self.y_base - y) / self.y_scale
 
         return int(xs + 0.5), int(ys + 0.5)
 
@@ -29,7 +30,7 @@ class _Transform:
         system for the overall model, (generally in 3D), to which all other model coordinates relate.
         """
 
-        x = xs * self.xscale + self.xbase
-        y = self.ybase - ys * self.yscale
+        x = xs * self.x_scale + self.x_base
+        y = self.y_base - ys * self.y_scale
 
         return x, y

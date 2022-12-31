c6acea168c9815a25fe71210d782816235e5aef4
Payton Pietron <paytonpietron@gmail.com>
2022-12-30 19:46:18 -0500

    Add docstrings to Oval

--- chapter0_provided_graphics/graphics/oval.py
+++ chapter0_provided_graphics/graphics/oval.py
@@ -2,19 +2,27 @@ from _bounding_box import _BoundingBox
 
 
 class Oval(_BoundingBox):
+    """An Oval object represents an oval graphic that can be drawn."""
+
     def __init__(self, point1, point2):
+        """Construct an oval in the bounding box determined by point1 and point2."""
+
         _BoundingBox.__init__(self, point1, point2)
 
     def clone(self):
+        """Return a clone of the object."""
+
         other = Oval(self.point1, self.point2)
         other.config = self.config.copy()
 
         return other
 
     def _draw(self, canvas, options):
-        p1 = self.point1
-        p2 = self.point2
-        x1, y1 = canvas.get_screen_coords(p1.x, p1.y)
-        x2, y2 = canvas.get_screen_coords(p2.x, p2.y)
+        """Draw the object in a GraphicsWindow."""
+
+        point1 = self.point1
+        point2 = self.point2
+        x1, y1 = canvas.get_screen_coords(point1.x, point1.y)
+        x2, y2 = canvas.get_screen_coords(point2.x, point2.y)
 
         return canvas.create_oval(x1, y1, x2, y2, options)

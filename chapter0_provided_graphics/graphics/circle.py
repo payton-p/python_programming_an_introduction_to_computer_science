a1b9f3dc6b30ba56a6a2db5ea7f585c050e9ee91
Payton Pietron <paytonpietron@gmail.com>
2022-12-30 19:12:34 -0500

    Add docstrings to Circle

--- chapter0_provided_graphics/graphics/circle.py
+++ chapter0_provided_graphics/graphics/circle.py
@@ -3,17 +3,25 @@ from point import Point
 
 
 class Circle(Oval):
+    """A Circle object represents a circle graphic that can be drawn."""
+
     def __init__(self, center, radius):
-        p1 = Point(center.x - radius, center.y - radius)
-        p2 = Point(center.x + radius, center.y + radius)
-        Oval.__init__(self, p1, p2)
+        """Construct a circle with given center point and radius."""
+
+        point1 = Point(center.x - radius, center.y - radius)
+        point2 = Point(center.x + radius, center.y + radius)
+        Oval.__init__(self, point1, point2)
         self.radius = radius
 
     def clone(self):
+        """Return a clone of the circle."""
+
         other = Circle(self.get_center(), self.radius)
         other.config = self.config.copy()
-        
+
         return other
 
     def get_radius(self):
+        """Return the radius of the circle."""
+
         return self.radius

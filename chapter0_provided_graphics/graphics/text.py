475d833cc74073407cf77e753de947304a7e875a
Payton Pietron <paytonpietron@gmail.com>
2022-12-30 20:19:34 -0500

    Add docstrings to Text and clean up code, missed file

--- chapter0_provided_graphics/graphics/text.py
+++ chapter0_provided_graphics/graphics/text.py
@@ -4,57 +4,100 @@ from _graphics_object import _GraphicsObject
 
 
 class Text(_GraphicsObject):
-    def __init__(self, p, text):
+    """A Text object represents a text graphic that can be drawn."""
+
+    def __init__(self, anchor_point, text):
+        """
+        Construct a text object that displays the given string centered at the anchor point. The text is displayed
+        horizontally.
+        """
+
         _GraphicsObject.__init__(self, ["justify", "fill", "text", "font"])
         self.set_text(text)
-        self.anchor = p.clone()
+        self.anchor = anchor_point.clone()
         self.set_fill(DEFAULT_CONFIG['outline'])
         self.set_outline = self.set_fill
 
-    def _draw(self, canvas, options):
-        p = self.anchor
-        x, y = canvas.get_screen_coords(p.x, p.y)
-
-        return canvas.create_text(x, y, options)
-
-    def _move(self, dx, dy):
-        self.anchor.move(dx, dy)
-
     def clone(self):
+        """Return a clone of the object."""
+
         other = Text(self.anchor, self.config['text'])
         other.config = self.config.copy()
 
         return other
 
     def set_text(self, text):
+        """Set the value of text."""
+
         self._reconfig("text", text)
 
     def get_text(self):
+        """Return the value of text."""
+
         return self.config["text"]
 
     def get_anchor(self):
+        """Return a clone of the anchor point."""
+
         return self.anchor.clone()
 
-    def set_face(self, face):
-        if face in ['helvetica', 'arial', 'courier', 'times roman']:
-            f, s, b = self.config['font']
-            self._reconfig("font", (face, s, b))
+    def set_font_face(self, typeface):
+        """
+        Change the font face to the given family.
+
+        Possible values are: "helvetica", "courier", "times roman", and "arial".
+        """
+
+        if typeface in ['helvetica', 'arial', 'courier', 'times roman']:
+            font_face, font_size, font_style = self.config['font']
+            self._reconfig("font", (typeface, font_size, font_style))
         else:
             raise _GraphicsError(BAD_OPTION_ERROR_MESSAGE)
 
-    def set_size(self, size):
+    def set_font_size(self, size):
+        """
+        Change the font size to the given point size.
+
+        Sizes from 5 to 36 are legal.
+        """
+
         if 5 <= size <= 36:
-            f, s, b = self.config['font']
-            self._reconfig("font", (f, size, b))
+            font_face, font_size, font_style = self.config['font']
+            self._reconfig("font", (font_face, size, font_style))
         else:
             raise _GraphicsError(BAD_OPTION_ERROR_MESSAGE)
 
-    def set_style(self, style):
+    def set_font_style(self, style):
+        """
+        Change font to the given style.
+
+        Possible values are "normal", "bold", "italic", and "bold italic".
+        """
+
         if style in ['bold', 'normal', 'italic', 'bold italic']:
-            f, s, b = self.config['font']
-            self._reconfig("font", (f, s, style))
+            font_face, font_size, font_style = self.config['font']
+            self._reconfig("font", (font_face, font_size, style))
         else:
             raise _GraphicsError(BAD_OPTION_ERROR_MESSAGE)
 
     def set_text_color(self, color):
+        """
+        Set the color of the text.
+
+        Note: set_fill has the same effect.
+        """
+
         self.set_fill(color)
+
+    def _draw(self, canvas, options):
+        """Draw the object in a GraphicsWindow."""
+
+        point = self.anchor
+        x, y = canvas.get_screen_coords(point.x, point.y)
+
+        return canvas.create_text(x, y, options)
+
+    def _move(self, dx, dy):
+        """Update internal state of object to move it dx,dy units."""
+
+        self.anchor.move(dx, dy)

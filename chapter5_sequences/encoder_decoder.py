d9a35781913ece264c24e26d4c97e47753317232
Payton Pietron <paytonpietron@gmail.com>
2023-01-01 19:14:23 -0500

    Add some additional comments

--- chapter5_sequences/encoder_decoder.py
+++ chapter5_sequences/encoder_decoder.py
@@ -1,18 +1,28 @@
 def decode():
+    """Convert a sequence of Unicode numbers into a string of text."""
+
     print("This program converts a sequence of Unicode numbers into the string of text that it represents.\n")
 
     encoded_message = input("Please enter the Unicode-encoded message: ")
-
-    # Convert a sequence of Unicode numbers into a string of text.
-    message = ""
+    chars = []
     for num_string in encoded_message.split():
         code_num = eval(num_string)
-        message = message + chr(code_num)
+        chars.append(chr(code_num))
 
+    # You could also solve it like this, but strings are immutable, while lists are mutable, so this solution is less
+    # efficient.
+    # message = ""
+    # for num_string in encoded_message.split():
+    #     code_num = eval(num_string)
+    #     message = message + chr(code_num)
+
+    message = "".join(chars)
     print("\nThe decoded message is:", message)
 
 
 def encode():
+    """Convert a textual message into a sequence of numbers, utilizing the underlying Unicode encoding."""
+
     print(
         "This program converts a textual message into a sequence of numbers representing the Unicode encoding of the "
         + "message.\n")
@@ -21,7 +31,6 @@ def encode():
 
     print("\nHere is the encoded message:")
 
-    # Convert a textual message into a sequence of numbers, utilizing the underlying Unicode encoding.
     for ch in message:
         print(ord(ch), end=" ")
 

bcb1664fd51fd69619d9afc5292e98db9e54087a
Payton Pietron <paytonpietron@gmail.com>
2023-01-01 20:36:50 -0500

    File processing examples

---
+++ chapter5_sequences/file_processing.py
@@ -0,0 +1,33 @@
+def read_file():
+    file = open("file_processing_example.txt", "r")  # open the file
+    text = file.read()  # read in the file contents
+    print(text)
+
+
+def read_and_write_file():
+    """This program creates a file of usernames from a file of names."""
+
+    # Open the files.
+    input_file = open("file_processing_names.txt", "r")
+    output_file = open("file_processing_usernames.txt", "w")
+
+    # Process each line of the input file and write to the output file.
+    for line in input_file:
+        first, last = line.split()
+        username = (first[0] + last[:7]).lower()  # create the username
+
+        print(username, file=output_file)  # write to the output file
+
+    # Close both files.
+    input_file.close()
+    output_file.close()
+
+    print("\nUsernames have been written to:", output_file)
+
+
+def main():
+    read_file()
+    read_and_write_file()
+
+
+main()

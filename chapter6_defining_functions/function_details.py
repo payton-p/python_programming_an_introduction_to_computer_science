3a7ab6c30824156b4506af996342ad3a860769d4
Payton Pietron <paytonpietron@gmail.com>
2023-01-10 19:13:13 -0500

    Add function notes

---
+++ chapter6_defining_functions/function_details.py
@@ -0,0 +1,13 @@
+def function_without_return_statement():
+    x = 2 + 3
+
+
+def main():
+    print(function_without_return_statement())  # prints None
+
+    # Python passes parameters by value (not by reference). Passing mutable objects is a unique case. If you are passing
+    # a mutable object, such as a list, to a function, and the function alters the object’s state, that state change
+    # will be visible to the caller when the function returns.
+
+
+main()

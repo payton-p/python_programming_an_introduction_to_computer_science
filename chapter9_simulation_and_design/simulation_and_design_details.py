fe0e21f781662899a3bcde70fc3c97661d60652d
Payton Pietron <paytonpietron@gmail.com>
2023-01-12 21:12:43 -0500

    Pseudo-random number generator example

---
+++ chapter9_simulation_and_design/simulation_and_design_details.py
@@ -0,0 +1,13 @@
+from random import random, randrange
+
+
+def main():
+    # Generates pseudo-random numbers.
+    print(random())
+    print(randrange(3, 9))
+
+    # This chapter discusses top-down and spiral designs.
+
+
+if __name__ == "__main__":
+    main()

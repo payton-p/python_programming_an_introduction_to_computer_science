6d7868ef5e7c5444bcb401baed9cbf1d452a3773
Payton Pietron <paytonpietron@gmail.com>
2023-01-17 21:48:54 -0500

    Lists examples

---
+++ chapter11_data_collections/lists.py
@@ -0,0 +1,18 @@
+def main():
+    # Python lists can mix data types.
+    number_list = ["Hey", 1, 2, 3, 3.0]
+    print(number_list)
+
+    # Example of a membership check.
+    print(3 in number_list)
+
+    # Python lists are dynamic (aka mutable).
+    number_list[0] = "Hello"
+    print(number_list)
+
+    number_list.append(13)
+    print(number_list)
+
+
+if __name__ == "__main__":
+    main()
